e562d4ab16912fa0226f884592c3359ea454686c
yumi <yumi@howler.space>
2017-07-09 14:01:16 -0400

    Updated configuration template

--- includes/config.template.php
+++ includes/config.template.php
@@ -1,17 +1,17 @@
 <?php
-// Root for hooYa
-// Not currently used. . . if it were then it would be easier
-// to move files around
-define("CONFIG_HOOYA_ROOT_PATH","/var/http/hooYa/");
 // A place for thumbnails and encoded video, relative to DOCUMENT_HEAD
 define("CONFIG_TEMPORARY_PATH", "/var/http/a/");
 define("CONFIG_TEMPORARY_WEBPATH", "/a/");
 
 // A path for stylsheets and logins
-define("CONFIG_COMMON_PATH", "/var/http/common/");
-define("CONFIG_COMMON_WEBPATH", "/common/");
+define("CONFIG_COMMON_PATH", "/var/http/hub/common/");
+define("CONFIG_COMMON_WEBPATH", "/hub/common/");
 
-// Tag databases for hooYa
-define("CONFIG_TAG_DB", "/path/to/tags.db");
-define("CONFIG_META_DB", "/path/to/meta.db");
+// Tag database for hooYa
+define("CONFIG_MYSQL_HOOYA_HOST", "localhost");
+define("CONFIG_MYSQL_HOOYA_USER", "MYSQL_USER");
+define("CONFIG_MYSQL_HOOYA_PASSWORD", "MYSQL_PASSWORD");
+define("CONFIG_MYSQL_HOOYA_DATABASE", "MYSQL_DB");
+
+define("CONFIG_HOOYA_STORAGE_PATH", "/var/media/")
 ?>
